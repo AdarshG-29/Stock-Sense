62045cba927cb99d08be0570d0a1a1ce8c7c976c
Adarsh Gupta <adarshg@groww.in>
2025-11-11 05:48:58 +0530

    check on home page

--- web/src/app/features/home/HomePage.tsx
+++ web/src/app/features/home/HomePage.tsx
@@ -1,13 +1,15 @@
 "use client"
-import { getHistoricalCandles } from '@/services/candles.api'
+import {  getHistoricalCandles, getTodayCandles } from '@/services/candles.api'
 import React, { useEffect } from 'react'
 
 const Home = () => {
   useEffect(() => {
     const fetchHistoricalCandles = async () => {
       try{
-        const res = await getHistoricalCandles()
-        console.log(res.data)
+        const res = await getTodayCandles()
+        console.log(res.data, 'Today Candles');
+        const res2 = await getHistoricalCandles();
+        console.log(res2.data, 'Historical Candles');
       }
    catch(error){
         console.error('Error fetching historical candles:', error)
