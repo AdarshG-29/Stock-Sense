cb08e8fa6263bd56bd9c6dc1239b27cca4c0579f
Adarsh Gupta <adarshg@groww.in>
2025-11-18 08:41:34 +0530

    search items implemented

--- web/src/app/features/home/HomePage.tsx
+++ web/src/app/features/home/HomePage.tsx
@@ -1,28 +1,39 @@
-"use client"
-import {  getHistoricalCandles, getTodayCandles } from '@/services/candles.api'
-import React, { useEffect } from 'react'
+"use client";
+
+import { Input } from "@/components/ui/input";
+import { useSearchItem } from "@/hooks/useSearchItem";
+import {  getHistoricalCandles } from "@/services/candles.api";
+import { SearchItemType } from "@/types/SearchItemType";
 
 const Home = () => {
-  useEffect(() => {
-    const fetchHistoricalCandles = async () => {
-      try{
-        const res = await getTodayCandles()
-        console.log(res.data, 'Today Candles');
-        const res2 = await getHistoricalCandles();
-        console.log(res2.data, 'Historical Candles');
-      }
-   catch(error){
-        console.error('Error fetching historical candles:', error)
-      }
+  const {searchInput, searchResults, onInputChange} = useSearchItem();
+
+  const onStockClick = async (item: SearchItemType) => {
+    try{
+      const res = await getHistoricalCandles(item?.instrument_key);
+      console.log("Today's Candles:", res?.data);
+    } catch (error) {
+      console.error("Error fetching today's candles:", error);
     }
-    fetchHistoricalCandles()
-  },[])
+  }
 
   return (
-    <div className='flex flex-col items-center justify-center text-center h-[60vh]'>
-      <h1 className='text-4xl font-bold'>Welcome to Stock Sense</h1>
+    <div className="flex flex-col items-center justify-center text-center h-[60vh]">
+      <h1 className="text-4xl font-bold">Welcome to Stock Sense</h1>
+      <Input
+        type="text"
+        placeholder="Search for stocks..."
+        className="mt-4 w-1/2"
+        value={searchInput}
+        onChange={(e) => onInputChange(e.target.value)}
+      />
+      {searchResults?.map((item) => (
+        <div key={item?.instrument_key} className="mt-2" onClick={() => onStockClick(item)}>
+          {item?.name}
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
