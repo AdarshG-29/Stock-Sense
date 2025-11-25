d4c85f965f8862ee3374ae20ac593b4695d423f8
Adarsh Gupta <adarshg@groww.in>
2025-11-25 07:00:41 +0530

    backtesting implemented fully

--- web/src/app/features/chartScreen/Chart.tsx
+++ web/src/app/features/chartScreen/Chart.tsx
@@ -1,103 +1,58 @@
 import { Spinner } from '@/components/ui/spinner';
 import { useInstrumentChartStore } from '@/stores/instrumentChart/store';
-import { useState } from 'react'
 import TradingViewChart from '../tradingViewChart/TradingViewChart';
-import { getLocalTimeStamp } from '@/utils/helper';
-import Dropdown from '@/components/dropdown/Dropdown';
-import { UTCTimestamp } from 'lightweight-charts';
-import { Button } from '@/components/ui/button';
 import { useBackTestStore } from '@/stores/backTestStore/store';
-import { setResetChart, setStartPlaying, setStopPlaying, setTimeStampIndex } from '@/stores/backTestStore/action';
+import { setResetChart, setStartPlaying, setStopPlaying } from '@/stores/backTestStore/action';
+import { PlayIcon, StopCircleIcon, RefreshCwIcon } from 'lucide-react';
 
 const Chart = () => {
-    const [timeStampInterval, setTimeStampInterval] = useState<UTCTimestamp | null>();
-
     const isBacktestEnable = useBackTestStore.use.isBacktestEnable();
-    const timeStampIndex = useBackTestStore.use.timeStampIndex();
-    const isPlaying = useBackTestStore.use.isPlaying();
-    const resetChart = useBackTestStore.use.resetChart();
-
     const ohlc = useInstrumentChartStore.use.ohlc();
     const isLoading = useInstrumentChartStore.use.isLoading();
     const isError = useInstrumentChartStore.use.isError();
 
-
-const onTimeStampIntervalChange = (timeStamp: string) => {
-    if(timeStamp.length===0) {
-        return;
-    }
-    const timeStampInterval = parseInt(timeStamp) as UTCTimestamp;
-    const currentIndex = ohlc.findIndex((item) => item.timeStamp === timeStampInterval);
-    setTimeStampInterval(timeStampInterval);
-    setTimeStampIndex(currentIndex);
-}
-
-const onStopReplay = () => {
-   setTimeStampInterval(null);
-   setStopPlaying();
-}
-
-const onResetChartClick = () => {
-    setTimeStampInterval(null);
-    setResetChart();
-}
-
     if (isLoading) {
         return (
-            <div className='w-100 flex justify-center items-center'>
-               <Spinner className='size-8'/>
+            <div className="w-100 flex justify-center items-center">
+                <Spinner className="size-8" />
             </div>
         );
     }
 
-    if(isError) {
+    if (isError) {
         return (
-            <div className='text-red-500 font-bold'>
+            <div className="text-red-500 font-bold">
                 Error loading chart data. Please try again.
             </div>
         );
     }
 
-  return (
-    <div className='flex flex-col gap-4'>
-        {
-        ohlc.length > 0 && isBacktestEnable &&
-        <div className='flex gap-4 items-end'>
-        <Dropdown
-            value={timeStampInterval ? timeStampInterval.toString(): ''}
-            options={ohlc ? ohlc.map((item) => ({
-                label: getLocalTimeStamp(item.timeStamp),
-                value: item.timeStamp.toString()
-            })) : []}
-            onValueChange={onTimeStampIntervalChange}
-            label="Timestamp Interval"
-        />
-        <Button variant="default" 
-        onClick={setStartPlaying} 
-        disabled={timeStampIndex===null}>
-            Start replay
-            </Button>
-        <Button variant="default" 
-        onClick={onStopReplay}
-        disabled={!isPlaying}>
-            Stop replay</Button>
-        <Button variant="default" 
-        onClick={onResetChartClick}
-        >Reset chart</Button>
-        </div>
-        }
-        <div className='bg-white p-4 rounded-lg shadow-md'>
-            <TradingViewChart
-            ohlcData={ohlc}
-            timeStampIndex={timeStampIndex}
-            isPlaying={isPlaying}
-            resetChart={resetChart}
-            stopReplay={onStopReplay}
-            isBacktestEnable={isBacktestEnable}
-            />
+    return (
+        <div className="flex w-full flex-col gap-4">
+            {ohlc.length > 0 && isBacktestEnable && (
+                <div className="w-full flex items-center justify-center gap-4 ml-auto">
+                    <PlayIcon
+                        className="w-8 h-8 text-green-500 font-bold cursor-pointer border border-gray-300 bg-black p-2 rounded"
+                        onClick={setStartPlaying}
+                        xlinkTitle="Start"
+                    />
+                    <StopCircleIcon
+                        className="w-8 h-8 text-red-500 font-bold cursor-pointer border border-gray-300 bg-black p-2 rounded"
+                        onClick={setStopPlaying}
+                        xlinkTitle="Stop"
+                    />
+                    <RefreshCwIcon
+                        className="w-8 h-8 text-blue-500 font-bold cursor-pointer border border-gray-300 bg-black p-2 rounded"
+                        onClick={setResetChart}
+                        xlinkTitle="Reset"
+                    />
+                </div>
+            )}
+            <div className="bg-white p-4 rounded-lg shadow-md">
+                <TradingViewChart />
             </div>
         </div>
-  )
-}
+    );
+};
 
-export default Chart
+export default Chart;
--- web/src/app/features/tradingViewChart/TradingViewChart.tsx
+++ web/src/app/features/tradingViewChart/TradingViewChart.tsx
@@ -1,275 +1,46 @@
 import { VOLUME_COLOR_CONSTANTS } from '@/constants/chart';
-import { OhlcCandleType } from '@/types/InstrumentChart';
-import {
-    formatOhlcDataIntoChartData,
-    formatVolumeDataIntoChartData,
-    getformattedVolume,
-    getLocalTimeStamp,
-} from '@/utils/helper';
-import {
-    createChart,
-    ColorType,
-    CandlestickSeries,
-    HistogramSeries,
-    ISeriesApi,
-} from 'lightweight-charts';
-import { useEffect, useRef, useState } from 'react';
+import { getformattedVolume } from '@/utils/helper';
+import { ScissorsIcon } from 'lucide-react';
+import { useTradingView } from './hooks/useTradingView';
 
-type Props = {
-    ohlcData: OhlcCandleType[];
-    timeStampIndex: number | null;
-    isPlaying: boolean;
-    resetChart: boolean;
-    stopReplay: () => void;
-    isBacktestEnable: boolean;
-};
+const TradingViewChart = () => {
+    const { volumeInfo, xPosition, chartContainerRef } = useTradingView();
 
-const TradingViewChart = (props: Props) => {
-    const [volumeInfo, setVolumeInfo] = useState<{ volume: number; color: string | undefined } | null>(null);
-    const [hoveredTime, setHoveredTime] = useState<number | null>(null);
-    const [xPosition, setXPosition] = useState<number | null>(null);
+    const { left = 0, right = 0 } = chartContainerRef.current?.getBoundingClientRect() || {};
+    const coveredWidth = right - left - (xPosition || 0) - (window.innerWidth - (right - 20));
 
-    const { isPlaying, timeStampIndex, ohlcData, stopReplay, resetChart, isBacktestEnable } = props;
-    
-    const playbackIntervalRef = useRef<NodeJS.Timeout | null>(null);
-    const chartContainerRef = useRef<HTMLDivElement>(null);
-    const chartRef = useRef<ReturnType<typeof createChart> | null>(null);
-    const volumeSeriesRef = useRef<ISeriesApi<'Histogram'> | null>(null);
-    const candleSeriesRef = useRef<ISeriesApi<'Candlestick'> | null>(null);
-
-    useEffect(() => {
-        if (!chartContainerRef.current) return;
-        
-        const chart = initializeChart();
-        setupCandlestickSeries();
-        setupVolumeSeries();
-        handleCrosshairMove();
-        addVerticalLineCrossshairMove();
-
-        const handleResize = () => {
-            if (chartContainerRef.current && chart) {
-                chart.applyOptions({ width: chartContainerRef.current.clientWidth });
-            }
-        };
-
-        window.addEventListener('resize', handleResize);
-
-        return () => {
-            window.removeEventListener('resize', handleResize);
-            setVolumeInfo(null);
-            clearIntervalIfExists();
-            removeChart();
-        };
-
-    },[ohlcData])
-
-    useEffect(() => {
-            handlePlayback();
-    },[isPlaying])
-
-    useEffect(() => {
-            popCandles();
-    }, [timeStampIndex]);
-
-    useEffect(() => {
-        resetCandles();
-    },[resetChart])
-
-    const initializeChart = () => {
-        const container = chartContainerRef.current;
-        if (!container) return;
-        const chart = createChart(container, {
-            layout: {
-                background: { type: ColorType.Solid },
-            },
-            width: container.clientWidth,
-            height: 800,
-        });
-        chart.timeScale().fitContent();
-        chart.applyOptions({
-            timeScale: {
-                timeVisible: true,
-                tickMarkFormatter: getLocalTimeStamp,
-            },
-            localization: {
-                timeFormatter: getLocalTimeStamp,
-            },
-        });
-        chartRef.current = chart;
-        return chart;
-    };
-
-    const removeChart = () => {
-        const chart = chartRef.current;
-        if (chart) {
-            chart.remove();
-            chartRef.current = null;
-        }
-    }
-
-    const setupCandlestickSeries = () => {
-        const chart = chartRef.current
-        if(!chart)
-            return;
-
-        const candlestickSeries = chart.addSeries(CandlestickSeries, {
-            priceScaleId: 'right',
-            priceFormat: {
-                type: 'price',
-            },
-        });
-        chart.priceScale('right').applyOptions({
-            scaleMargins: {
-                top: 0.1,
-                bottom: 0.4,
-            },
-        });
-        candlestickSeries.setData(ohlcData.map(formatOhlcDataIntoChartData));
-        candleSeriesRef.current = candlestickSeries;
-        return candlestickSeries;
-    };
-
-    const setupVolumeSeries = () => {
-        const chart = chartRef.current
-        if(!chart)
-            return;
-
-        const volumeSeries = chart.addSeries(HistogramSeries, {
-            priceScaleId: 'volume',
-            priceFormat: {
-                type: 'volume',
-            },
-        });
-        chart.priceScale('volume').applyOptions({
-            scaleMargins: {
-                top: 0.8,
-                bottom: 0,
-            },
-        });
-        volumeSeries.setData(ohlcData.map(formatVolumeDataIntoChartData));
-        volumeSeriesRef.current = volumeSeries;
-        return volumeSeries;
-    };
-
-    const handleCrosshairMove = (
-    ) => {
-        const chart = chartRef.current;
-        const volumeSeries = volumeSeriesRef.current;
-
-        if(!chart || !volumeSeries)
-            return;
-
-        chart.subscribeCrosshairMove((param) => {
-            if (param.seriesData.size) {
-                const volume = param.seriesData.get(volumeSeries);
-                if (volume && 'value' in volume && volume.value > 0) {
-                    setVolumeInfo({ volume: volume.value, color: volume.color });
-                }
-            }
-        })
-    };
-
-    const clearIntervalIfExists = () => {
-        if (playbackIntervalRef.current) {
-            clearInterval(playbackIntervalRef.current);
-        }
-    };
-
-    const popCandles = () => {
-        const candleSeries = candleSeriesRef.current;
-        const volumeSeries = volumeSeriesRef.current;
-
-        if(!candleSeries || !volumeSeries || timeStampIndex === null) return;
-        const popCount = candleSeries.data().length - timeStampIndex - 1;
-        if(popCount < 0) return;
-        candleSeries.pop(popCount);
-        volumeSeries.pop(popCount);
-    }
-
-    const handlePlayback = () => {
-        const candleSeries = candleSeriesRef.current;
-        const volumeSeries = volumeSeriesRef.current;
-
-        if (!isPlaying || timeStampIndex === null || !candleSeries || !volumeSeries) {
-            clearIntervalIfExists();
-            return;
-        }
-
-        let currentIndex = timeStampIndex;
-        playbackIntervalRef.current = setInterval(() => {
-
-            if (currentIndex >= ohlcData.length - 1) {
-                clearIntervalIfExists();
-                currentIndex = 0;
-                stopReplay();
-                return;
-            }
-            const newData = ohlcData[currentIndex + 1];
-            candleSeries.update(formatOhlcDataIntoChartData(newData));
-            volumeSeries.update(formatVolumeDataIntoChartData(newData));
-            currentIndex += 1;
-        }, 500);
-    };
-
-    const resetCandles = () => {
-        const candleSeries = candleSeriesRef.current;
-        const volumeSeries = volumeSeriesRef.current;
-
-        if(!candleSeries || !volumeSeries || !resetChart) return;
-        clearIntervalIfExists();
-        candleSeries.setData(ohlcData.map(formatOhlcDataIntoChartData));
-        volumeSeries.setData(ohlcData.map(formatVolumeDataIntoChartData));
-    }
-
-    const addVerticalLineCrossshairMove = () => {
-        const chart = chartRef.current;
-        const candleSeries = candleSeriesRef.current;
-
-        if (!chart || !candleSeries) return;
-
-        chart.subscribeCrosshairMove((param) => {
-            const {logical, point} = param;
-            if(
-                logical === undefined || 
-                point === undefined ||
-                 logical<0 ||
-                  logical >= candleSeries.data().length || 
-                  !isBacktestEnable
-                ) {
-                setHoveredTime(null);
-                setXPosition(null);
-                return;
-            }
-                setHoveredTime(logical);
-                setXPosition(point.x);
-        });
-    };
     return (
         <div ref={chartContainerRef} className="relative w-full h-full">
             {volumeInfo?.volume && (
-            <div className="absolute top-5 left-1/2 transform -translate-x-1/2 z-10 font-bold text-sm">
-                <span>Vol: </span>
-                <span
-                className="font-semibold"
-                style={{
-                    color:
-                    volumeInfo?.color === VOLUME_COLOR_CONSTANTS.BAR_UP_COLOR
-                        ? VOLUME_COLOR_CONSTANTS.TEXT_UP_COLOR
-                        : VOLUME_COLOR_CONSTANTS.TEXT_DOWN_COLOR,
-                }}
-                >
-                {getformattedVolume(volumeInfo.volume)}
-                </span>
-            </div>
+                <div className="absolute top-5 left-1/2 transform -translate-x-1/2 z-10 font-bold text-sm">
+                    <span>Vol: </span>
+                    <span
+                        className="font-semibold"
+                        style={{
+                            color:
+                                volumeInfo?.color === VOLUME_COLOR_CONSTANTS.BAR_UP_COLOR
+                                    ? VOLUME_COLOR_CONSTANTS.TEXT_UP_COLOR
+                                    : VOLUME_COLOR_CONSTANTS.TEXT_DOWN_COLOR,
+                        }}
+                    >
+                        {getformattedVolume(volumeInfo.volume)}
+                    </span>
+                </div>
             )}
-            {
-            hoveredTime && xPosition !== null && isBacktestEnable &&(
-                <div
-                className="absolute top-0 h-full w-[1px] pointer-events-none"
-                style={{ left: xPosition, top: 0, bottom: 0, border: '1px solid blue', zIndex: 10 }}
-                />
-            )
-            }
+            {xPosition !== null && (
+                <>
+                    <div
+                        className="absolute top-0 h-full w-[1px] pointer-events-none border border-blue-700 z-10"
+                        style={{ left: xPosition }}
+                    >
+                        <ScissorsIcon className="absolute top-1/2 -translate-x-1/2 text-blue-500" />
+                    </div>
+                    <div
+                        className="absolute top-0 h-full pointer-events-none z-10 bg-black bg-opacity-35"
+                        style={{ left: xPosition, width: coveredWidth }}
+                    />
+                </>
+            )}
         </div>
     );
 };
